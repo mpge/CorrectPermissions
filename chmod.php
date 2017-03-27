bdf2462f637dc1ca7ce8c7b52887ae1789ab6f4e
Matthew Paul Gross <MatthewGross@users.noreply.github.com>
2017-03-27 13:11:06 -0400

    adding variables so user can easily change permissions

--- chmod.php
+++ chmod.php
@@ -1,16 +1,19 @@
 <?php
 
-function chmod_r($Path) {
+$file_permissions = 0644;
+$dir_permissions = 0750;
+
+function chmod_r($Path, $file_permissions, $dir_permissions) {
     $dp = opendir($Path);
      while($File = readdir($dp)) {
        if($File != "." AND $File != "..") {
          if(is_dir($File)){
             echo 'chmod '.$Path.'/'.$File.' to 0750<br>';
-            chmod($File, 0750);
+            chmod($File, $dir_permissions);
             chmod_r($Path."/".$File);
          }else{
              echo 'chmod '.$Path.'/'.$File.' to 0644<br>';
-             chmod($Path."/".$File, 0644);
+             chmod($Path."/".$File, $file_permissions);
          }
        }
      }
@@ -18,6 +21,6 @@ function chmod_r($Path) {
 }
 
 $current_dir = getcwd();
-chmod_r($current_dir);
+chmod_r($current_dir, $file_permissions, $dir_permissions);
 
 ?>
